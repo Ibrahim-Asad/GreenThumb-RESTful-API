4a999630eb3e21eb6c5627535f1f6c0b3b655d6e
Ibrahim-Asad <ibrahim.sadi.asad@gmail.com>
2024-06-06 19:46:22 +0300

    exception handling

--- src/main/java/com/greenthumb/security/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/greenthumb/security/service/impl/CustomUserDetailsService.java
@@ -1,7 +1,5 @@
-package com.greenthumb.security.service;
+package com.greenthumb.security.service.impl;
 
-import com.greenthumb.security.model.dto.UserCreateDTO;
-import com.greenthumb.security.model.dto.UserDTO;
 import com.greenthumb.security.model.entity.UserEntity;
 import com.greenthumb.security.model.mapper.UserEntityMapper;
 import com.greenthumb.security.repository.UserRepo;
@@ -9,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
